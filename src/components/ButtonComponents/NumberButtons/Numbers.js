41f4467ae1f3524a3eb17030c35a51a9379db508
Raudel Flores <rawdell@outlook.com>
2019-11-15 00:01:39 -0600

    imported necessary components in each files and created the useState within each function to import the data from arrays

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { numbers } from '../../../data';
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -9,6 +10,7 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [num, setNum] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { operators } from '../../../data';
 
 //import any components needed
 
@@ -6,6 +7,7 @@ import React from "react";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [char, setChar] = useState(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
