e8acb4d361038d5ad49a814822d071d7739f6892
Raudel Flores <rawdell@outlook.com>
2019-11-16 22:08:37 -0600

    Created the button in the SpecialButton file then exported into the Special.js file to execute the data. Applied similar to the other button files

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { specials } from '../../../data';
+import Special from './SpecialButton';
 //import any components needed
 
 //Import your array data to from the provided data file
@@ -12,6 +13,11 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {special.map((key, index) => {
+         return <Special key={index} spec={key}/>
+       })}
     </div>
   );
 };
+
+export default Specials;
