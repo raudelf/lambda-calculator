e511e2c4db9bca8748a7f146a963dbee15c18339
Raudel Flores <rawdell@outlook.com>
2019-11-16 21:19:29 -0600

    Created the button code in Operator Button.js and currently working to import it on the Operators.js while making it work

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { operators } from '../../../data';
+import OperatorButton from './OperatorButton';
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+const Operators = (props) => {
   // STEP 2 - add the imported data to state
   const [char, setChar] = useState(operators);
   return (
@@ -13,6 +14,11 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {char.map(character, value => {
+         return <OperatorButton />
+       })}
     </div>
   );
 };
+
+export default Operators;
