a133ad28f3df8e184f739672a31a862ff625b4f6
Raudel Flores <rawdell@outlook.com>
2019-11-16 20:40:43 -0600

    figured out what was wrong with my code. Got the buttons to display by having numsign with an index of 0 (ZERO). Then imported the number button and applied the props parameter to link it together

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import Number from './components/ButtonComponents/NumberButtons/Numbers';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
-
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
@@ -18,7 +18,8 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-      </div>
+        </div>
+        <Number />
     </div>
   );
 }
